a8a2ad5d94a24b1512dba1d16677c3a624dd8521
Nikolai <nikolai_sergeevich@mail.ru>
2021-12-17 18:36:49 +0300

    create error, footer, nav components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,16 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { NavComponent } from './components/nav/nav.component';
+import { ErrorComponent } from './components/error/error.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ErrorComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/error/error.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.scss']
+})
+export class ErrorComponent {
+
+  constructor(
+    public router: Router
+  ) { }
+  
+  public goToHome(): void {
+    this.router.navigate(["/home"])
+  };
+
+}
